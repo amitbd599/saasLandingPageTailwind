a6cfbbe734070db8a63fbd12109aab12228ba948
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2023-09-21 03:13:37 +0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1rem",
+        md: "2rem",
+        lg: "2rem",
+        xl: "2rem",
+        "2xl": "5rem",
+      },
+    },
+    screens: {
+      sm: "380px",
+      // => @media (min-width: 380px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      colors: {
+        theme: "#52BD94",
+        hover: "#30946D",
+        p: "#A6A6A6",
+        h: "#191A15",
+      },
+    },
+  },
+  plugins: [],
+};
